3b9108919df56b542b9e4eab81a2b620fee84f79
Lee Jung Hun <duddlswnddb@naver.com>
2025-11-13 14:31:30 +0900

    Time: 183 ms (57.8%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,5 @@
+-- Write your PostgreSQL query statement below
+select case when id % 2 = 1 and id=(select max(id) from seat) then id
+when id % 2 = 1 then id+1
+when id % 2 = 0 then id-1 end id, student from seat
+order by id;
