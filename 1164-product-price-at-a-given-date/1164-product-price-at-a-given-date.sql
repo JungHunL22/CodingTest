c63ee432812376bdc8200682cb14018cd84b5fc1
Lee Jung Hun <duddlswnddb@naver.com>
2025-11-13 16:27:57 +0900

    Time: 369 ms (51.52%), Space: 0B (100%) - LeetHub

---
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -0,0 +1,11 @@
+-- Write your PostgreSQL query statement below
+-- select * from products where (product_id,change_date) in (select product_id,max(change_date) from products
+-- group by 1
+-- having change_date<='2019-08-16');
+with temp1 as (
+select * from products 
+where (product_id,change_date) in
+(select product_id,max(change_date) from products
+where change_date<='2019-08-16'
+group by 1))
+select distinct a.product_id, coalesce(b.new_price,10) price from products a left join temp1 b on a.product_id=b.product_id
