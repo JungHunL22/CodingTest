e2668f0e15d0a4cbb1b59a3011653e7e7b9ce314
Lee Jung Hun <duddlswnddb@naver.com>
2025-11-13 15:53:18 +0900

    Time: 679 ms (11.76%), Space: 0B (100%) - LeetHub

---
+++ 1158-market-analysis-i/1158-market-analysis-i.sql
@@ -0,0 +1,16 @@
+-- Write your PostgreSQL query statement below
+-- with temp1 as (
+--     select buyer_id,count(item_id) cnt from orders 
+-- where extract('year' from order_date)=2019 group by buyer_id
+-- )
+-- select user_id buyer_id, join_date,
+-- case when cnt is null then 0
+-- else cnt end orders_in_2019 
+-- from users a left join temp1 b on a.user_id=b.buyer_id
+-- ;
+select user_id as buyer_id,
+join_date,
+count(*) filter (where extract(year from order_date)=2019) as orders_in_2019
+from users a left join orders b on a.user_id=b.buyer_id
+group by 1,2
+order by 1;
